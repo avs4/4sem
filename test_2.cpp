bf2ee45e30f7d9ccf79ba65189e804f46e6da678
Igor Kuritsyn <kio1996@mail.ru>
2016-12-09 20:37:22 +0400

    Update test_2.cpp

--- test_2.cpp
+++ test_2.cpp
@@ -1,1 +1,21 @@
+#include "stdio.h"
+#include "stdlib.h"
 
+void main()
+{
+	int a[3],b;
+	FILE *f;
+
+	fopen_s(&f,"qaz.txt","r");
+	for (int i=0; i<=2; i++)
+		fscanf(f,"%u",&a[i]);
+	fclose(f);
+
+	b=a[0]+a[1]+a[2];
+
+	fopen_s(&f,"wsx.txt","w");
+	fprintf(f,"%u",b);
+	fclose(f);
+
+	system("pause");
+}
